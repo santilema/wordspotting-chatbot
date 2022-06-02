b3e83a98390d2c5ff95d8ec8538abc02da0cdc15
Gulasal Kuchkarova <gulasal.kuchkarova@stud.th-deg.de>
2022-06-02 21:32:49 +0200

    ui created

--- README.md
+++
@@ -1,2 +0,0 @@
-# hospital_chatbot
-chatbot
--- chatbot-ui/src/App.js
+++ chatbot-ui/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
+import Chatbot from 'react-chatbot-kit'
+import 'react-chatbot-kit/build/main.css'
+
+import config from './config';
+import MessageParser from './MessageParser';
+import ActionProvider from './ActionProvider';
+
+import   './App.css'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div style={{maxWidth : "300px"}}>
+        <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+        />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
