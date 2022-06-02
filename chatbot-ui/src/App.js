cfd2f79d8f2f2f56c254906efacf64631e7dc964
Gulasal Kuchkarova <gulasal.kuchkarova@stud.th-deg.de>
2022-06-03 00:50:52 +0200

    ui build

--- chatbot-ui/src/App.js
+++ chatbot-ui/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import image from './images/background_img.jpeg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapper'>
+        <div className='content'>
+          <div className='header'>
+            <div className='img'>
+              <img src={image} alt='background'/>
+            </div>
+            <div className='right'>
+              <div className='name'>Chatbot</div>
+              <div className='status'>Active</div>
+            </div>
+          </div>
+          <div className='main'>
+            <div className='main_content'>
+              <div className='messages'>
+                <div className='bot-messages' id='message1'></div>
+                <div className='human-messages' id='message2'></div>
+              </div>
+            </div>
+          </div>
+          <div className='bottom'>
+            <div className='bottom-side'>
+              <input type='text' id='input' placeholder='Enter your message' />
+              <div className='button'>
+                <button className='btn'><i class="fa-solid fa-paper-plane"></i> Send</button> 
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
