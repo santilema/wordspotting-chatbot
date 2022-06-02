31e2ff0a2c77d0aab6c71f801c7f2f3369fcbb3e
Bahtiyorjon Arslonbekov <bahtiyorjon.arslonbekov@stud.th-deg.de>
2022-06-02 22:32:45 +0200

    initial state

--- chatbot-ui/src/App.js
+++ chatbot-ui/src/App.js
@@ -1,28 +1,25 @@
-
-import React from 'react'
-import Chatbot from 'react-chatbot-kit'
-import 'react-chatbot-kit/build/main.css'
-
-import config from './config';
-import MessageParser from './MessageParser';
-import ActionProvider from './ActionProvider';
-
-import   './App.css'
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <div style={{maxWidth : "300px"}}>
-        <Chatbot
-          config={config}
-          messageParser={MessageParser}
-          actionProvider={ActionProvider}
-        />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
