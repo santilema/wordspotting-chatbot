20dd446670302accf755b56f5c2b00711d55f15f
santi <lema.sx@gmail.com>
2022-06-19 16:59:40 +0200

    further changes on the conversation structure

--- server/answers.js
+++ server/answers.js
@@ -1,4 +1,4 @@
-const availableDates = ["05.07.2022", "06.07.2022"]
+const availableDates = ['05/07/2022', '06/07/2022']
 const availableDepartments = ["Cardiology", "Neurology", "Paedriatics", "Psychiatry"]
 
 const conversation = {
@@ -10,28 +10,44 @@ const conversation = {
         level: 0,
         conversationFlow: [
             {
-            sentenceId: "appo",
-            content: "Would you like to book an appointment?",
-            expect: ["appointment", "consultation", "booking", "book", "date", "session", "when", "available", "free", "week", "month"],
-            level: 1,
-            direct: "appo-type",
-            conversationFlow: [
-                {
-                    sentenceId: "appo-type",
-                    content: `The following departments are taking online turns:\n${availableDepartments}\nFor which would you like to book yout appointment?`,
-                    expect: ["yes", "please", "sure", "ok", "okay"],
-                    level: 2,
-                    direct: "appo-avail",
-                    conversationFlow: [
-                        {
-                            sentenceId: "appo-avail",
-                            content: `These are the available dates:\n${availableDates}`,  // define available dates and departments list
-                            expect: availableDepartments,
-                            conversationFlow: [] // write further conversation
-                        }
-                    ]
-                }
-            ]
+                sentenceId: "loc",
+                content: "The hospital is located on Example Street 123. Attention 24 hours for emergencies. Please, consult opening hours for specific departments",
+                expect: ["street", "location", "where", "entrance", "access", "open", "opening", "schedule", "direction", "address"],
+                level: 1,
+
+            },
+            {
+                sentenceId: "appo",
+                content: "Would you like to book an appointment?",
+                expect: ["appointment", "consultation", "booking", "book", "date", "session", "when", "available", "free", "week", "month"],
+                level: 1,
+                conversationFlow: [
+                    {
+                        sentenceId: "appo-type",
+                        content: `The following departments are taking online turns:\n${availableDepartments}\nFor which would you like to book yout appointment?`,
+                        expect: ["yes", "please", "sure", "ok", "okay"],
+                        level: 2,
+                        conversationFlow: [
+                            {
+                                sentenceId: "appo-avail",
+                                content: `These are the available dates:\n${availableDates}\nPlease, select one date`,
+                                expect: availableDepartments,
+                                conversationFlow: [
+                                    {
+                                        sentenceId: "appo-email",
+                                        content: `Great! To finish with your booking I'll have to ask you for an email`,
+                                        expect: availableDates,
+                                        conversationFlow: [
+                                    {
+                                        
+                                    }
+                                ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
             }
         ]
     };
