c3ae44a9a7e4c3b157362cc396c144cb5db067f8
sl10098 <santiago.lema@stud.th-deg.de>
2022-06-30 16:20:00 +0200

    Bot now handles situations with multiple requests

--- server/answers.js
+++ server/answers.js
@@ -1,29 +1,31 @@
+const prompt = require('prompt-sync')();
 const availableDates = ['05/07/2022', '06/07/2022']
-const availableDepartments = ["Cardiology", "Neurology", "Paedriatics", "Psychiatry"]
+const availableDepartments = ["cardiology", "neurology", "paedriatics", "psychiatry"]
 
-const conversation = {
+const conversationRaw = {
         sentencecId: "base",
         mainPhrase: "Hi my name is Hospi, how can I help you?",
-        secondPhtase: "Is there anything else I can help with?",
+        secondPhrase: "Is there anything else I can help with?",
         thirdPhrase: "Sorry, I don't know how to do that. Is there anything else I can help you with?",
         expect: [],
         level: 0,
         conversationFlow: [
             {
                 sentenceId: "pick-up",
-                content: "Do you have to pick-up some study-results?",
-                expect: ["pick", "up", "collect", "results", "result"],
+                mainPhrase: "Do you have to pick-up some study results?",
+                secondPhrase: "You have to collect some results, is that right?",
+                expect: ["pick", "up", "pickup", "collect", "results", "result", "study", "studies"],
                 level: 1,
                 conversationFlow: [
                     {
                         sentenceId: "pick-up-verify",
-                        content: "please, write down your email to check if your results are available already",
+                        mainPhrase: "please, write down your email to check if your results are available already",
                         expect: ["yes", "please", "sure", "ok", "okay", "affirmative", "y", "yeah", "exactly", "yep", "right"],
                         level: 2,
                         conversationFlow: [
                             { 
                                 sentenceId: "pick-up-success",
-                                content: "Your results are ready. You can pick them up on reception from 8:00 to 17:00 everyday",
+                                mainPhrase: "Your results are ready. You can pick them up on reception from 8:00 to 17:00 everyday",
                                 //expect: email
                                 level: 3,
                                 direct: "base" 
@@ -34,7 +36,7 @@ const conversation = {
             },
             {
                 sentenceId: "loc",
-                content: "The hospital is located on Example Street 123. Attention 24 hours for emergencies. Please, consult opening hours for specific departments",
+                mainPhrase: "The hospital is located on Example Street 123. Attention 24 hours for emergencies. Please, consult opening hours for specific departments",
                 expect: ["street", "location", "located", "where", "entrance", "access", "open", "opening", "schedule", "direction", "address"],
                 level: 1,
                 direct: "base"
@@ -42,31 +44,32 @@ const conversation = {
             },
             {
                 sentenceId: "appo",
-                content: "Would you like to book an appointment?",
+                mainPhrase: "Do you want to book an appointment?",
+                secondPhrase: "All right, so we are booking an appointment then, is that ok?",
                 expect: ["appointment", "consultation", "booking", "book", "date", "session", "when", "available", "free", "week", "month"],
                 level: 1,
                 conversationFlow: [
                     {
                         sentenceId: "appo-type",
-                        content: `The following departments are taking online turns:\n${availableDepartments}\nFor which would you like to book yout appointment?`,
+                        mainPhrase: `The following departments are taking online turns:\n${availableDepartments}\nFor which would you like to book yout appointment?`,
                         expect: ["yes", "please", "sure", "ok", "okay", "affirmative", "y", "yeah", "exactly", "yep", "right"],
                         level: 2,
                         conversationFlow: [
                             {
                                 sentenceId: "appo-avail",
-                                content: `These are the available dates:\n${availableDates}\nPlease, select one date`,
+                                mainPhrase: `These are the available dates:\n${availableDates}\nPlease, select one date`,
                                 expect: availableDepartments,
                                 level: 3,
                                 conversationFlow: [
                                     {
                                         sentenceId: "appo-email",
-                                        content: `Great! To finish with your booking I'll have to ask you for an email`,
+                                        mainPhrase: `Great! To finish with your booking I'll have to ask you for an email`,
                                         expect: availableDates,
                                         level: 4,
                                         conversationFlow: [
                                             {
                                                 sentenceId: "appo-success",
-                                                content: "Your appointment was registered :)",
+                                                mainPhrase: "Your appointment was registered :)",
                                                 //expect: email
                                                 level: 5,
                                                 direct: "base"
@@ -82,7 +85,12 @@ const conversation = {
         ]
     };
 
-function talk(conver, userMessage, iteration){
+const misunderstanding = {
+    sentenceId: 'mis',
+
+};
+
+function talk(flow, userMessage, ite){
     const messageWords = userMessage.toLowerCase().replace(/[,.?!]/,"").split(' '); //converts message into array of words
     let depth = 0;
     let heighestScore = 0;
@@ -90,8 +98,8 @@ function talk(conver, userMessage, iteration){
     let altPath;
     let responseMsg
 
-    for (let index = 0; index < conver.conversationFlow.length; index++) {
-        currentObject = conver.conversationFlow[index];
+    for (let index = 0; index < flow.length; index++) {
+        currentObject = flow[index];
         let score = 0;
         messageWords.forEach(word => {
             if (currentObject.expect.includes(word)) {
@@ -99,36 +107,53 @@ function talk(conver, userMessage, iteration){
             }
         });
         if (score > heighestScore) {
-            selectedPath = conver.conversationFlow[index];
+            selectedPath = currentObject;
             heighestScore = score;
             if (typeof altPath !== "undefined") {
                 altPath = undefined;
             }
-        } else if (score === heighestScore && typeof selectedPath !== "undefined") {
-            altPath = selectedPath;
-            selectedPath = conver.conversationFlow[index]
+        } else if (score === heighestScore && score !== 0) {
+            altPath = currentObject;
         }
     }
     if (heighestScore > 0) {
+        ite = 0;
         if (typeof altPath !== "undefined") {
             selectedPath.expect.push("first", "1", "1st", "one", "a");
             altPath.expect.push("second", "two", "2", "2nd", "b");
-            responseMsg = `I didn't get that very clearly,\n${selectedPath.content} or\n${altPath.content}`;
+            responseMsg = `I can just handle one thing at once :(\n${selectedPath.mainPhrase} or\n${altPath.mainPhrase}`;
+            selectedPath.mainPhrase = selectedPath.secondPhrase;
+            altPath.mainPhrase = altPath.secondPhrase;
+            conver = { conversationFlow: flow }
         } else {
-            responseMsg = selectedPath.content;
-            conver = selectedPath.conversationFlow;
+            responseMsg = selectedPath.mainPhrase;
+            conver = selectedPath;
+        }
+    } else {
+        if (ite < 2) {
+            responseMsg = "I didn't get that. Please, try to reformulate"
+            conver = { conversationFlow: flow }
+            ite += 1;
+        } else {
+            conver = conversationRaw;
+            responseMsg = conversationRaw.thirdPhrase;
         }
     }
 
     return {
         updatedConversation: conver,
-        outMessageText: responseMsg
+        outMessageText: responseMsg,
+        iteration: ite,
     };
 }
 
-function someFancyLogic(inMessage) {
-    const responseObject = talk(conversation, inMessage, 0);
+function someFancyLogic(inMessage, conversationState, iteration) {
+    const responseObject = talk(conversationState.conversationFlow, inMessage, iteration);
     console.log(responseObject.outMessageText);
+    let nextMessage = prompt("Your response: ");
+    // console.log(responseObject.updatedConversation)
+    someFancyLogic(nextMessage, responseObject.updatedConversation, responseObject.iteration)
 }
 
-someFancyLogic("I have to pick up some studies");
+const firstMessage = prompt("Your query: ");
+someFancyLogic(firstMessage, conversationRaw, 0);
