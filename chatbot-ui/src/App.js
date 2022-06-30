946108feec79c4b4ca1dc0f6de70ba56a4b871f3
bakhtee <bahtiyorjon.arslonbekov@stud.th-deg.de>
2022-06-30 16:03:05 +0200

    merge request from master to bahti

--- chatbot-ui/src/App.js
+++ chatbot-ui/src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 import Collector from './components/collector/collector';
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import io from "socket.io-react";
+
+const socket = io.connect('hhtp://localhost:3000')
 
 function App() {
-    const [size, setSize] = useState(true)
+  const [username, setUsername] = useState('')
+  const [message, setMessage] = useState("")
+  const [messages, setMessages] = useState([]);
+  const [size, setSize] = useState(true)
+
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      socket.emit("username", username)
+    })
+  })
     const handleClick = () => {
       setSize(!size)
     }
     
     return (
-      <div className="app">
+      <div className="app" socket={socket}>
          <Collector size = {size} handleClick = {handleClick} />
       </div>
     );
--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const port = 3000;
+const port = 5000;
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    // res.sendFile(__dirname + '/index.html');
+    res.json({"users" : ["baba", "bibi"]});
 });
+
+// this method creates a unique userID
+const getUserID = () => {
+    const s4 = () => {
+        Math.floor((1 + Math.random()) * 0*10000).toString(16).substring(1)
+    }
+    return s4() + s4() + "-" + s4()
+}
 
 io.on('connection', (socket) => {
     console.log('Client connected');
