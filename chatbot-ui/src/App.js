f85103a87d112028fab3c37ea4c6b223c79ad253
santi <lema.sx@gmail.com>
2022-12-28 19:37:30 +0100

    Small changes for testing deploy to Azure

--- chatbot-ui/src/App.js
+++ chatbot-ui/src/App.js
@@ -26,7 +26,7 @@ function App() {
   const [size, setSize] = useState(false); // to resize the chat body
   const [messages, setMessages] = useState([
     {
-      text: "Hello, I am the Hospital ChatBot, how can I help You?",
+      text: "Hello, I am the Bank ChatBot, how can I help You?",
       position: "left",
       list: []
     },
